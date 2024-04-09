b838529c4fbc1aa7cf37695b72713c23312d9053
Bartlomiej Pilch <bartlomiejpilch@soogaco.pl>
2024-04-09 20:12:32 +0200

    first activity

---
+++ app/src/main/java/soogaco/app/medstopquestionnaire/view/splash/SplashActivity.java
@@ -0,0 +1,16 @@
+package soogaco.app.medstopquestionnaire.view.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import soogaco.app.medstopquestionnaire.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
