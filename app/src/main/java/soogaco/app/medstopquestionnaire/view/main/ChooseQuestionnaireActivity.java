b36eb8eb4940a912f1569ed1ce7d1e88199de789
Bartlomiej Pilch <bartlomiejpilch@soogaco.pl>
2024-04-13 19:19:53 +0200

    splash screen

---
+++ app/src/main/java/soogaco/app/medstopquestionnaire/view/main/ChooseQuestionnaireActivity.java
@@ -0,0 +1,16 @@
+package soogaco.app.medstopquestionnaire.view.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import soogaco.app.medstopquestionnaire.R;
+
+public class ChooseQuestionnaireActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_questionnaire);
+    }
+}
