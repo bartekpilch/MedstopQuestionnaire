24a23fb1218ec08a393f21a85f0ea45178efc576
Bartlomiej Pilch <bartlomiejpilch@soogaco.pl>
2024-04-14 20:28:38 +0200

    first questionary

---
+++ app/src/main/java/soogaco/app/medstopquestionnaire/view/main/QuestionnaireActivity.java
@@ -0,0 +1,16 @@
+package soogaco.app.medstopquestionnaire.view.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import soogaco.app.medstopquestionnaire.R;
+
+public class QuestionnaireActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questionnaire);
+    }
+}
--- app/src/main/java/soogaco/app/medstopquestionnaire/view/splash/SplashActivity.java
+++ app/src/main/java/soogaco/app/medstopquestionnaire/view/splash/SplashActivity.java
@@ -2,15 +2,31 @@ package soogaco.app.medstopquestionnaire.view.splash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import soogaco.app.medstopquestionnaire.R;
+import soogaco.app.medstopquestionnaire.view.main.QuestionnaireActivity;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_DISPLAY_LENGTH = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                // Po zakończeniu czasu, uruchom główną aktywność
+                Intent mainIntent = new Intent(SplashActivity.this, QuestionnaireActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+
     }
 }
